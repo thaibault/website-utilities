785e333fff3da101473ca6539015eac2d78a4dff
thaibault <t.sickert@gmail.com>
2013-11-01 20:15:56 +0100

    Update project homepage content.

--- distributionBundle/jquery-website-1.0.js
+++ distributionBundle/jquery-website-1.0.js
@@ -152,7 +152,7 @@
       Website.prototype.__googleAnalyticsCode = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{1}', 'github.io');ga('send', 'pageview');";
 
       /**
-          @description Initializes the interactive web app.
+          @description Initializes the interactive web application.
       
           @param {Object} options An options object.
       
