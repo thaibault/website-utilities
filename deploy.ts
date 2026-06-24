65190b3fef6b5061b862e242d17d019995aa2557
thaibault <t.sickert@gmail.com>
2026-06-24 16:18:19 +0200

    Automatic update to new version.

--- deploy.ts
+++ deploy.ts
@@ -32,58 +32,56 @@ export const log =
 const run = (command: string, options = {}): string =>
     execSync(command, {encoding: 'utf-8', shell: '/bin/bash', ...options})
 
-if (run('git branch').includes('* main')) {
-    log.info('Build new web page.')
-    log.info(run('yarn clear'))
-    log.info(run('yarn build'))
+log.info('Build new web page.')
+log.info(run('yarn clear'))
+log.info(run('yarn build'))
 
-    if (!await isFile(resolve(PUBLIC_REPOSITORY_PATH, 'CNAME')))
-        throw new Error(
-            `Missing public website directory in "${PUBLIC_REPOSITORY_PATH}"`
-        )
-
-    log.info(`Update page data in "${PUBLIC_REPOSITORY_PATH}".`)
-
-    await removeDirectoryRecursively(
-        PUBLIC_REPOSITORY_PATH,
-        {filter: (path) =>
-            Promise.resolve(
-                ![
-                    PUBLIC_REPOSITORY_PATH,
-                    resolve(PUBLIC_REPOSITORY_PATH, '.git'),
-                    resolve(PUBLIC_REPOSITORY_PATH, '.github'),
-                    resolve(PUBLIC_REPOSITORY_PATH, 'CNAME'),
-                    resolve(PUBLIC_REPOSITORY_PATH, 'readme.md'),
-                    resolve(PUBLIC_REPOSITORY_PATH, 'public-repository')
-                ].some((ignorePath) => path.startsWith(ignorePath))
-            )
-        }
+if (!await isFile(resolve(PUBLIC_REPOSITORY_PATH, 'CNAME')))
+    throw new Error(
+        `Missing public website directory in "${PUBLIC_REPOSITORY_PATH}"`
     )
 
-    await copyDirectoryRecursive('build', PUBLIC_REPOSITORY_PATH, true)
+log.info(`Update page data in "${PUBLIC_REPOSITORY_PATH}".`)
 
-    log.info(run('yarn clear'))
-
-    log.info('Upload compiled webpage')
-
-    if (process.env.USER_NAME_GITHUB) {
-        log.info(`Set git user name to "${process.env.USER_NAME_GITHUB}".`)
-        log.info(run(
-            `git config --global user.name '${process.env.USER_NAME_GITHUB}'`,
-            {cwd: PUBLIC_REPOSITORY_PATH}
-        ))
+await removeDirectoryRecursively(
+    PUBLIC_REPOSITORY_PATH,
+    {filter: (path) =>
+        Promise.resolve(
+            ![
+                PUBLIC_REPOSITORY_PATH,
+                resolve(PUBLIC_REPOSITORY_PATH, '.git'),
+                resolve(PUBLIC_REPOSITORY_PATH, '.github'),
+                resolve(PUBLIC_REPOSITORY_PATH, 'CNAME'),
+                resolve(PUBLIC_REPOSITORY_PATH, 'readme.md'),
+                resolve(PUBLIC_REPOSITORY_PATH, 'public-repository')
+            ].some((ignorePath) => path.startsWith(ignorePath))
+        )
     }
-    if (process.env.USER_EMAIL_GITHUB) {
-        log.info(`Set git user email to "${process.env.USER_EMAIL_GITHUB}".`)
-        log.info(run(
-            `git config --global user.email '${process.env.USER_EMAIL_GITHUB}'`,
-            {cwd: PUBLIC_REPOSITORY_PATH}
-        ))
-    }
+)
 
+await copyDirectoryRecursive('build', PUBLIC_REPOSITORY_PATH, true)
+
+log.info(run('yarn clear'))
+
+log.info('Upload compiled webpage')
+
+if (process.env.USER_NAME_GITHUB) {
+    log.info(`Set git user name to "${process.env.USER_NAME_GITHUB}".`)
     log.info(run(
-        `git commit --all --message 'Automatic page build update.'`,
+        `git config --global user.name '${process.env.USER_NAME_GITHUB}'`,
         {cwd: PUBLIC_REPOSITORY_PATH}
     ))
-    log.info(run('git push', {cwd: PUBLIC_REPOSITORY_PATH}))
 }
+if (process.env.USER_EMAIL_GITHUB) {
+    log.info(`Set git user email to "${process.env.USER_EMAIL_GITHUB}".`)
+    log.info(run(
+        `git config --global user.email '${process.env.USER_EMAIL_GITHUB}'`,
+        {cwd: PUBLIC_REPOSITORY_PATH}
+    ))
+}
+
+log.info(run(
+    `git commit --all --message 'Automatic page build update.'`,
+    {cwd: PUBLIC_REPOSITORY_PATH}
+))
+log.info(run('git push', {cwd: PUBLIC_REPOSITORY_PATH}))
